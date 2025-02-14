4ebe53dc226f201619615a20b41b0ab28b1a3535
kevinnatalini <kevinrugby333@gmail.com>
2025-02-15 00:20:33 +0100

    builder

--- MedicalShifts/obj/Debug/net9.0/MedicalShifts.AssemblyInfo.cs
+++ MedicalShifts/obj/Debug/net9.0/MedicalShifts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedicalShifts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3995e674ffa64c8e888a96b2e3b7873d0a048121")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fee36006083d1d12781897d5360c8821da3314e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedicalShifts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedicalShifts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
